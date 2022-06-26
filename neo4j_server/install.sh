24d4e66eca568b26bd59aee2652e7abd39e3d886
jeffrey82221 <jeffrey82221@gmail.com>
2022-06-26 16:18:38 +0000

    [feature] neo4j install

---
+++ neo4j_server/install.sh
@@ -0,0 +1,4 @@
+wget --no-check-certificate -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
+apt update
+apt install neo4j
