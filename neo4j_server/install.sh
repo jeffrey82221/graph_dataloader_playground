c1c6703e269444c5872a1e80f62508b948fe2f38
jeffrey82221 <jeffrey82221@gmail.com>
2022-06-29 08:49:19 +0000

    [upgrade] neo4j install script (3.5.5 -> 4.8.8x.x)

--- neo4j_server/install.sh
+++ neo4j_server/install.sh
@@ -1,4 +1,8 @@
-wget --no-check-certificate -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
-echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
+wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb https://debian.neo4j.com stable latest' | sudo tee /etc/apt/sources.list.d/neo4j.list
 apt update
 apt install neo4j
+# NOTE: 
+# Community Edition May Cause Error: Property existence constraint requires Neo4j Enterprise Edition
+# - CREATE CONSTRAINT constraint_edge IF NOT EXISTS ON ()-[p:Label]-() ASSERT EXISTS (p.Key); 
+# -> Unable to create Constraint( type='RELATIONSHIP PROPERTY EXISTENCE', schema=-[:Label {Key}]- ):
