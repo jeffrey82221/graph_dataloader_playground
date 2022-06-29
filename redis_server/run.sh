2455fd5b9d02b232e0b3328a8365629aebf04a61
jeffrey82221 <jeffrey82221@gmail.com>
2022-06-29 10:37:26 +0000

    [fix] script

--- redis_server/run.sh
+++ redis_server/run.sh
@@ -1,2 +1,1 @@
-# /usr/local/lib/python3.7/site-packages/redis_server/bin/redis-server --port 6379 --loadmodule /usr/local/lib/redisgraph/redisgraph.so
 /usr/local/lib/python3.6/site-packages/redis_server/bin/redis-server --port 6379 --loadmodule /usr/local/lib/redisgraph/redisgraph.so
