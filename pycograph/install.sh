6cbf3876944dbc423f523774fc6014e073f236e9
jeffrey82221 <jeffrey82221@gmail.com>
2022-07-01 03:20:15 +0000

    [feature] add pycograph installation and running guide

---
+++ pycograph/install.sh
@@ -0,0 +1,8 @@
+# Installation of Python3.8
+
+# Start Virtual Environment of Python3.8 
+
+# Install pycograph 
+
+# Start RedisGraph 
+
---
+++ pycograph/run.sh
@@ -0,0 +1,4 @@
+# Extract Code Graph into RedisGraph 
+
+# Test content in RedisGraph using Sqerzo
+
