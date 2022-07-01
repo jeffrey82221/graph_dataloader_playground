bb5439b8fc447bc7348e54ae2a06286f8ba10f3f
jeffrey82221 <jeffrey82221@gmail.com>
2022-07-01 06:21:04 +0000

    feature] add python3.8 / pyccograph installation guide and running and checking guide

--- pycograph/install.sh
+++ pycograph/install.sh
@@ -1,8 +1,13 @@
 # Installation of Python3.8
-
+apt-get install software-properties-common
+add-apt-repository -y ppa:deadsnakes/ppa
+apt update
+apt install python3.8
+apt install python3.8-venv
 # Start Virtual Environment of Python3.8 
-
+python3.8 -m venv env
+source env/bin/activate
 # Install pycograph 
-
-# Start RedisGraph 
-
+pip install pycograph
+# Start or Restart RedisGraph 
+/usr/local/lib/python3.6/site-packages/redis_server/bin/redis-server --port 6379 --loadmodule /usr/local/lib/redisgraph/redisgraph.so
--- pycograph/run.sh
+++ pycograph/run.sh
@@ -1,4 +1,12 @@
 # Extract Code Graph into RedisGraph 
-
-# Test content in RedisGraph using Sqerzo
-
+cd <the directory of interest>
+pycograph load --project-dir .
+# Test content in RedisGraph using its python package
+```python
+import redis
+from redisgraph import Graph
+r = redis.Redis(host='localhost', port=6379)
+redis_graph = Graph('.', r) 
+# "." can be replace by the folder name of the codebase
+redis_graph.query('MATCH (n) RETURN n').pretty_print()
+```
