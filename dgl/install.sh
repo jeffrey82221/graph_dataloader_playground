c0c18bcaa148869fb99498c20c8ab57b33697b57
jeffrey82221 <jeffrey82221@gmail.com>
2022-07-01 09:22:56 +0000

    [feature] add instalation guide of cuda version dgl and pytorch

--- dgl/install.sh
+++ dgl/install.sh
@@ -1,3 +1,13 @@
-pip install dgl dglgo -f https://data.dgl.ai/wheels/repo.html
-git clone --recurse-submodules -j8 https://github.com/RedisGraph/RedisGraph.git
-apt-get install build-essential cmake m4 automake peg libtool autoconf
+# (Experimental) Downgrade CUDA from 11.1 to 11.0 to check if DGL and Pytorch cu11.3 work on CUDA 11.0 
+# only do this on colab
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_amd64.deb
+dpkg -i cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_amd64.deb
+apt-key add /var/cuda-repo-ubuntu1804-11-0-local/7fa2af80.pub
+apt-get update
+apt-get install cuda-11.0
+# Install Pytorch for CUDA 11.3
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+# Install DGL for CUDA 11.3
+pip install dgl-cu113 dglgo -f https://data.dgl.ai/wheels/repo.html
