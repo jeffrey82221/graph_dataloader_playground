427a1949c6e77abb71233edcad8bf662573bfbd6
jeffrey82221 <jeffrey82221@gmail.com>
2022-07-03 12:39:11 +0800

    Update run.sh

--- redis_server/run.sh
+++ redis_server/run.sh
@@ -3,6 +3,7 @@ redis-stack-server
 # Running RedisInsight from Source
 git clone https://github.com/RedisInsight/RedisInsight.git
 cd RedisInsight
+yarn --cwd redisinsight/api/
 # 1. Run BackEnd - port: 5000
 yarn --cwd redisinsight/api/ start:dev
 # 2. Run FrontEnd - port: 8080
