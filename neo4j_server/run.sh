472dc7f038b5f780820da6066aca3a08e8ed3003
jeffrey82221 <jeffrey82221@gmail.com>
2022-07-03 04:14:49 +0000

    [doc] add default port

--- neo4j_server/run.sh
+++ neo4j_server/run.sh
@@ -1,3 +1,5 @@
+# Start neo4j at port 7687
+# & neo4j browser at port 7474 
 neo4j start
 
 """
--- redis_server/run.sh
+++ redis_server/run.sh
@@ -1,9 +1,9 @@
-# Run Redis-Stack-Server (with RedisGraph inside)
+# Run Redis-Stack-Server (with RedisGraph inside) - port: 6379
 redis-stack-server
 # Running RedisInsight from Source
 git clone https://github.com/RedisInsight/RedisInsight.git
 cd RedisInsight
-# 1. Run BackEnd
+# 1. Run BackEnd - port: 5000
 yarn --cwd redisinsight/api/ start:dev
-# 2. Run FrontEnd
+# 2. Run FrontEnd - port: 8080
 yarn start:web
