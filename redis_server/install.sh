00a21c38a17584280157d29b4591ceaac04eaf95
jeffrey82221 <jeffrey82221@gmail.com>
2022-07-03 03:19:47 +0000

    [doc] simplify install of redisgraph (using redis-stack-server)

--- redis_server/install.sh
+++ redis_server/install.sh
@@ -1,17 +1,16 @@
-# Install RedisServer v4.0.9
-apt install redis-server
-# Install RedisGraph 2.8.15 from source
-# NOTE: another way to update RedisGraph is to download the tagged released zip file
-# and move it into Aicloud for build. 
-cd /content/
-apt-get install build-essential cmake m4 automake peg libtool autoconf
-git clone --recurse-submodules -j8 https://github.com/RedisGraph/RedisGraph.git
-cd RedisGraph
-make
-# pip install -r tests/requirements.txt
-cp -r src /usr/local/lib/redisgraph
-cd ..
-rm -rf RedisGraph
+# Install Redis Stack (includes Redis 7.0.0 / RedisInsight / RedisGraph 2.8.12)
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+apt-get update
+apt-get install redis-stack-server
 # Install redis-server 6.0.9
 pip install redis-server
-# TODO: Install RedisInsight from source
+# Install redisinsight (not working)
+curl -O https://downloads.redisinsight.redislabs.com/latest/redisinsight-linux64
+mkdir /usr/local/redisinsight
+mv redisinsight-linux64-1.12.0 /usr/local/redisinsight/redisinsight-1.12.0
+chmod +x /usr/local/redisinsight/redisinsight-1.12.0
+nohup /usr/local/redisinsight/redisinsight-1.12.0
+echo "export REDISINSIGHT_HOST=127.0.0.1" >> ~/.bash_profile
+echo "export REDISINSIGHT_HOST_DIR=/usr/local/redisinsight/.redisinsight" >> ~/.bash_profile
+source ~/.bash_profile
